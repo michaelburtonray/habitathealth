a17d7a12e22d78712c5f623bed98b28eccf6dff9
Rinaldy <26579000+rinaldylaguerre@users.noreply.github.com>
2025-03-24 22:42:28 -0400

    type fix

--- app/(website)/[slug]/page.jsx
+++ app/(website)/[slug]/page.jsx
@@ -6,6 +6,8 @@ export default async function Page({ params }) {
   const { slug } = await params;
   const pageData = await getPageData({ slug, lang: 'en'});
 
+  console.log(pageData);
+
   return (
     <main className={`${slug}`}>
       {pageData && pageData.content && <Content content={pageData.content} title={pageData?.title || ''} />}
