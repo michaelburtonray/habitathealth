d61105496e561d6d16e1b901d4ace4270be3c47e
Rinaldy <26579000+rinaldylaguerre@users.noreply.github.com>
2025-04-24 17:48:46 -0400

    remove white line in header on mobile

--- components/Header.jsx
+++ components/Header.jsx
@@ -113,46 +113,48 @@ export default function Header(props) {
       <div className="hh-weglot--dynamic promo-bar rte body--small flex max-lg:flex-col lg:gap-2 items-center justify-end lg:justify-center h-[3.75rem] lg:h-10 max-lg:mb-4 relative text-white">
         {promoBar && <PortableText value={promoBar} components={components} />}
       </div>
-      <div className="bg-white flex xl:grid gap-8 xl:grid-cols-3 items-center justify-between min-h-20 px-5 max-lg:mt-4 lg:px-10 py-5 relative rounded-t-2xl text-green z-30">
-        <Link href="/" className="h-8">
-          <Logo />
-        </Link>
+      <div className="overflow-hidden relative text-green z-30">
+        <div className="bg-white flex xl:grid gap-8 xl:grid-cols-3 items-center justify-between min-h-20 max-lg:mt-4 px-5 lg:px-10 py-5 rounded-t-2xl w-full">
+          <Link href="/" className="h-8">
+            <Logo />
+          </Link>
 
-        <nav className="hh-weglot--dynamic nav-desktop body--large flex items-center justify-center max-xl:hidden">
-          <ul className="flex gap-8">
-            {nav.map((link) => (
-                <li
-                  key={link._key}
-                  className={`${link.internalLink && link.internalLink.slug === pathname.slice(1) ? 'active' : ''}`}
-                >
-                  <LinkObject {...link} />
-                </li>
-            ))}
-          </ul>
-        </nav>
+          <nav className="hh-weglot--dynamic nav-desktop body--large flex items-center justify-center max-xl:hidden">
+            <ul className="flex gap-8">
+              {nav.map((link) => (
+                  <li
+                    key={link._key}
+                    className={`${link.internalLink && link.internalLink.slug === pathname.slice(1) ? 'active' : ''}`}
+                  >
+                    <LinkObject {...link} />
+                  </li>
+              ))}
+            </ul>
+          </nav>
 
-        <div className="desktop-switcher buttons flex gap-3 max-xl:hidden  items-center justify-end">
-          <select
-            name="language-picker"
-            id="languages"
-            className="select select--lang"
-            value={currentLang}
-            onChange={switchLanguage}
-          >
-            {availableLanguages.map(({ value, text }) => {
-              return <option key={value} value={value}>{text}</option>
-            })}
-          </select>
+          <div className="desktop-switcher buttons flex gap-3 max-xl:hidden  items-center justify-end">
+            <select
+              name="language-picker"
+              id="languages"
+              className="select select--lang"
+              value={currentLang}
+              onChange={switchLanguage}
+            >
+              {availableLanguages.map(({ value, text }) => {
+                return <option key={value} value={value}>{text}</option>
+              })}
+            </select>
 
-          {cta && <Link href={cta.internalLink ? `/${cta.internalLink.slug}` : cta.url} className="hh-weglot--dynamic button button--green !min-h-10 !px-5">{cta.title || cta.internalLink.title}</Link>}
-        </div>
+            {cta && <Link href={cta.internalLink ? `/${cta.internalLink.slug}` : cta.url} className="hh-weglot--dynamic button button--green !min-h-10 !px-5">{cta.title || cta.internalLink.title}</Link>}
+          </div>
 
-        <div className="hh-weglot--dynamic menu flex items-center xl:hidden">
-          <button
-            onClick={() => setMenuIsOpen(!menuIsOpen)}
-          >
-            {menuIsOpen ? <IconClose /> : <IconHamburger />}
-          </button>
+          <div className="hh-weglot--dynamic menu flex items-center xl:hidden">
+            <button
+              onClick={() => setMenuIsOpen(!menuIsOpen)}
+            >
+              {menuIsOpen ? <IconClose /> : <IconHamburger />}
+            </button>
+          </div>
         </div>
       </div>
 
@@ -163,7 +165,7 @@ export default function Header(props) {
               animate={{ opacity: 1 }}
               exit={{ opacity: 0 }}
               initial={{ opacity: 0 }}
-              className="bg-cream px-[--padding] top-[6.375rem] py-10 rounded-b-[--radius] sticky bottom-10 overflow-scroll max-h-[calc(100svh-11.875rem)]"
+              className="bg-cream px-[--padding] top-[6.325rem] py-10 rounded-b-[--radius] sticky bottom-10 overflow-scroll max-h-[calc(100svh-11.875rem)]"
             >
               <nav className="nav-mobile">
                 <ul className="hh-weglot--dynamic flex flex-col gap-3">
